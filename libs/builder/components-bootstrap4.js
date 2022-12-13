f958a6cfeb98a50b3ce7f4ce586af439fce72f64
Tirthalg <tirthal2002@gmail.com>
2022-12-13 18:51:31 +0530

    5

--- libs/builder/components-bootstrap4.js
+++ libs/builder/components-bootstrap4.js
@@ -1538,10 +1538,10 @@ Vvveb.Components.extend("_base", "html/progress", {
 Vvveb.Components.extend("_base", "html/jumbotron", {
     classes: ["jumbotron"],
     image: "icons/jumbotron.svg",
-    name: "Jumbotron",
+    name: "Landing Page",
     html: '<div class="jumbotron">\
 		  <h1 class="display-3">Hello, world!</h1>\
-		  <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>\
+		  <p class="lead">This is a simple hero unit, a simple landing page component for calling extra attention to featured content or information.</p>\
 		  <hr class="my-4">\
 		  <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>\
 		  <p class="lead">\
